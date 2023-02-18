f1d64c9565aa130888fdf4fa5d9ce3d2ae3aead9
Tássio <tassio72@hotmail.com>
2023-02-18 13:29:25 -0300

    chore: create exec

---
+++ packages/extensions/src/executors/build/executor.ts
@@ -0,0 +1,23 @@
+import { writeFileSync, readFileSync } from 'fs';
+import { ExecutorContext } from 'nx/src/config/misc-interfaces';
+import { BuildExecutorSchema } from './schema';
+
+export default async function runExecutor(
+  options: BuildExecutorSchema,
+  context: ExecutorContext
+) {
+  const pathOutupt = 'packages/' + context.projectName + '/release.config.js';
+  const webConfigTemplatePath = __dirname + '/files/release.config__template__';
+  const webConfigTemplate = readFileSync(webConfigTemplatePath, 'utf8');
+
+  writeFileSync(
+    pathOutupt,
+    webConfigTemplate
+      .toString()
+      .replaceAll('{{PROJECT_NAME}}', context.projectName)
+  );
+
+  return {
+    success: true,
+  };
+}
---
+++ packages/extensions/src/generators/extensions/schema.d.ts
@@ -0,0 +1,5 @@
+export interface ExtensionsGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
