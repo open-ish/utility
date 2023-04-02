d261ed549ec507bf2244b53a101770bdc7e72185
Tássio <tassio72@hotmail.com>
2023-04-02 19:52:18 -0300

    chore: add params description

--- packages/trycatchfy/src/lib/helpers/trycatchfy.ts
+++ packages/trycatchfy/src/lib/helpers/trycatchfy.ts
@@ -4,6 +4,18 @@ import { httpMappedErrorsHelper } from './mappedErrors';
 
 const unExecutableFunction = () => null;
 
+/**
+ *
+  @param expectedBehavior is the expected behavior of the http request. Usually what we put on the try block of the trycatch.
+  @param onUnauthorizedError is the callback function called on 401 http request errors request.
+  @param onForbiddenError is the callback function called on 403 http request errors request.
+  @param onResourceError is the callback function called on 404 http request errors request.
+  @param onInternalServerError is the callback function called on any 5** http request errors request.
+  @param onHttpExceptionError is the callback function called on not mapped http errors.
+  @param onScriptError is the callback function called when there are javascript errors. It might happen when it gets a 200 status code, but the contract has been changed.
+  @param onEndCycle is the callback function called in the finally block of trycatch.
+onScriptError
+*/
 export const trycatchfy = async <IAxiosErrorResponse>(
   params: ITrycatchfyParams<IAxiosErrorResponse>
 ): Promise<void | Error> => {
