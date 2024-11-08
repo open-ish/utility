14c400cdda9a60609344096041646456f6aa7de7
tassioFront <tassio72@hotmail.com>
2024-11-08 14:16:06 -0300

    chore: remove breaking task

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,5 @@ module.exports = {
   '{packages,tools}/**/*.{ts, js, json, md, html, css,scss}': [
     'nx affected:lint --uncommited --fix true --base=origin/master --head=HEAD',
     'nx affected:test --base=origin/master --head=HEAD',
-    'nx format:write --base=origin/master --head=HEAD --uncommited',
   ],
 };
